6f685ea89266881ebf4d91bc57ebb3225c9ae1d6
Atemyn <atemyn@mail.ru>
2020-11-25 20:46:29 +0300

    Работа с экземплярами класса

--- PR_Lab_5.java
+++ PR_Lab_5.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import myClasses.Facade;
+import myClasses.Building;
+
 public class PR_Lab_5{
 	public static void main(String[] args){
-		
+		/* Работа с экземплярами класса. */
+		Building firstBuilding = new Building();
+		Building secondBuilding = new Building();
+
+		System.out.println("Программа по созданию зданий.\nВсе размерные данные вводятся в метрах.\n");
+		System.out.println("Работа с экземплярами класса: ");
+
+		firstBuilding.inputBuilding();
+		secondBuilding.initBuilding();
+
+		firstBuilding.addToBuilding(secondBuilding);
+
+		firstBuilding.addFloors();
+		firstBuilding.removeFloors();
+		/*............................................*/
 	}
 }
